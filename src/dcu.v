d5a8b8b8b1cc1e0d1c4fd620133f4e5ea8b9013e
Zhuo Nengwen <soarowl@yeah.net>
2023-12-25 21:38:24 +0800

    Compiler version traslated.

--- src/dcu.v
+++ src/dcu.v
@@ -2,15 +2,39 @@ module main
 
 import os
 
+enum Compiler as u8 {
+	delphi6    = 0x0E
+	delphi7    = 0x0F
+	delphi2005 = 0x11
+	delphi2006 = 0x12
+	delphi2009 = 0x14
+	delphi2010 = 0x15
+	delphixe   = 0x16
+	delphixe2  = 0x17
+	delphixe3  = 0x18
+	delphixe4  = 0x19
+	delphixe5  = 0x1A
+	delphixe6  = 0x1B
+	delphixe7  = 0x1C
+	delphixe8  = 0x1D
+	delphi10   = 0x1E
+	delphi10_1 = 0x1F
+	delphi10_2 = 0x20
+	delphi10_3 = 0x21
+	delphi10_4 = 0x22
+	delphi11   = 0x23
+	delphi12   = 0x24
+}
+
 pub struct Dcu {
 mut:
 	path string
 	data []u8
 	pos  usize
 
-	version          u32
-	compiler_version u8
-	platform         u8
+	version  u32
+	compiler Compiler
+	platform u8
 
 	size u32
 	date u32
@@ -24,7 +48,9 @@ pub fn (mut d Dcu) decompile(path string) ! {
 }
 
 fn (mut d Dcu) decode() ! {
-	d.compiler_version = d.data[3]
+	unsafe {
+		d.compiler = Compiler(d.data[3])
+	}
 	d.platform = d.data[1]
 	d.version = d.get[u32]()!
 	d.size = d.get[u32]()!
@@ -47,8 +73,30 @@ fn (d Dcu) get[T]() !T {
 }
 
 fn (d Dcu) write_file() ! {
+	version_map := {
+		Compiler.delphi6: 'Borland Delphi 6'
+		.delphi7:         'Borland Delphi 7'
+		.delphi2005:      'Borland Delphi 2005'
+		.delphi2006:      'Borland Delphi 2006'
+		.delphi2009:      'Codegear Delphi 2009'
+		.delphi2010:      'Codegear Delphi 2010'
+		.delphixe:        'Embarcadero Delphi XE'
+		.delphixe2:       'Embarcadero Delphi XE2'
+		.delphixe3:       'Embarcadero Delphi XE3'
+		.delphixe4:       'Embarcadero Delphi XE4'
+		.delphixe5:       'Embarcadero Delphi XE5'
+		.delphixe6:       'Embarcadero Delphi XE6'
+		.delphixe7:       'Embarcadero Delphi XE7'
+		.delphixe8:       'Embarcadero Delphi XE8'
+		.delphi10:        'Embarcadero Delphi 10 Seattle'
+		.delphi10_1:      'Embarcadero Delphi 10.1 Berlin'
+		.delphi10_2:      'Embarcadero Delphi 10.2 Tokyo'
+		.delphi10_3:      'Embarcadero Delphi 10.3 Rio'
+		.delphi11:        'Embarcadero Delphi 11 Alexandria'
+		.delphi12:        'Embarcadero Delphi 12 Athens'
+	}
 	mut buffer := '// version: ${d.version:08X}
-// compiler: ${d.compiler_version}
+// compiler: ${version_map[d.compiler]}
 // platform: ${d.platform}
 // size: ${d.size}
 // compile date: ${d.date}
