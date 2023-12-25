e902b8477a743dc44e4b0ad4f028998896d17a43
Zhuo Nengwen <soarowl@yeah.net>
2023-12-25 19:33:05 +0800

    version added.

--- src/dcu.v
+++ src/dcu.v
@@ -7,7 +7,12 @@ mut:
 	path    string
 	data    []u8
 	pos     usize
+
 	version u32
+	compiler_version u8
+	platform u8
+
+	size usize
 }
 
 pub fn (mut d Dcu) decompile(path string) ! {
@@ -20,11 +25,14 @@ fn (mut d Dcu) decode() ! {
 	unsafe {
 		d.version = *(&u32(&d.data[d.pos]))
 	}
+	d.compiler_version = d.data[3]
+	d.platform = d.data[1]
+	d.pos += 4
 
 	d.write_file()!
 }
 
 fn (d Dcu) write_file() ! {
-	mut buffer := '// ${d.version: 08X}'
+	mut buffer := '// version: ${d.version: 08X}'
 	os.write_file(d.path + '.pas', buffer)!
 }
