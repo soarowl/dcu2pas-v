637249dbfec0b8f6daa92f5654464ff38f47724f
Zhuo Nengwen <soarowl@yeah.net>
2023-12-25 10:14:32 +0800

    Command line option added.

--- src/main.v
+++ src/main.v
@@ -1,5 +1,37 @@
 module main
 
+import prantlf.cli { Cli, run }
+
+const version = '0.0.1'
+
+const usage = 'Decompile dcu(Delphi Compiled Unit) to pas.
+
+Usage: dcu2pas [options] [<dcu-file>]
+
+<dcu-file>                  Decompiled unit file.
+
+Options:
+  -V|--version              print the version of the executable and exits
+  -h|--help                 print the usage information and exits
+
+Examples:
+  $ dcu2pas demo.dcu test.dcu
+  $ dcu2pas -V'
+
+struct Opts {
+}
+
 fn main() {
-	println('Hello World!')
+	run(Cli{
+		usage: usage
+		version: version
+	}, body)
+}
+
+fn body(opts &Opts, args []string) ! {
+	if args.len > 0 {
+		for arg in args {
+			println(arg)
+		}
+	}
 }
